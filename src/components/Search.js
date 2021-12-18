c0abbbf582288ef708085eca3dd2167bd9f57935
Francis Dolan <francis.dolan@gmail.com>
2021-12-18 13:54:48 +0000

    Made search section a form landmark

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,21 +2,18 @@ import icon_search from '../assets/images/icon-search.svg'
 
 export default function Search(props) {
     return (
-        <section className="search">
-            <h2 className="search_title sr-only">Search</h2>
+        <form className="search" onSubmit={props.onSubmit}>
             <img className="search__icon" src={ icon_search } alt="Search"/>
-            <form className="search__form" onSubmit={props.onSubmit}>
-                <input
-                    className="search__form__input"
-                    type="text"
-                    aria-label="Search"
-                    name="username"
-                    placeholder="Search GitHub username.."
-                    onChange={ props.clearError }
-                />
-                { props.error && <div className="search__form__errors">No results</div> }
-                <input className="search__form__submit" type="submit" value="Search" />
-            </form>
-        </section>
+            <input
+                className="search__input"
+                type="text"
+                aria-label="Search"
+                name="username"
+                placeholder="Search GitHub username.."
+                onChange={ props.clearError }
+            />
+            { props.error && <div className="search__errors">No results</div> }
+            <input className="search__submit" type="submit" value="Search" />
+        </form>
     );
 }
